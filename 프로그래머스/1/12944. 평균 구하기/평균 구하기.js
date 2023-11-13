f2c699c31db501d1935ca825e8dd386904443a0d
zmjjkk98 <146824635+zmjjkk98@users.noreply.github.com>
2023-11-13 09:54:48 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,7 +1,3 @@
 function solution(arr) {
-    let arr2 = arr.reduce((acc, cur) => acc + cur, 0)/ arr.length;
-    
-    console.log(arr2)
-    var answer = arr2;
-    return answer;
+    return arr.reduce((acc, cur) => acc + cur, 0)/ arr.length;;
 }
