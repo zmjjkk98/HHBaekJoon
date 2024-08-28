2f724182b194b7e5ff192b51beff8e7d8c8fe5c0
zmjjkk98 <146824635+zmjjkk98@users.noreply.github.com>
2024-08-28 23:24:31 +0900

    [level 1] Title: 콜라 문제, Time: 0.36 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,14 +1,9 @@
 function solution(a, b, n) {
     var answer = 0;
-    let rest = 0;
     
-    while(n>=a){
-        rest = n%a
-        n = (Math.floor(n/a) * b);
-        answer += n
-        n += rest
-        if(n < a) break;
-        
+    while(n>=a){     
+        answer += (Math.floor(n/a) * b)
+        n = (Math.floor(n/a) * b) + (n%a)
         
     }
 
